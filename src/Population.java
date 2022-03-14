df286e019dbfd2d2bbba7677cc654bbdd381d1fa
JasLivie <101596915+JasLivie@users.noreply.github.com>
2022-03-14 16:36:18 -0400

    ajouter public ... en general

--- src/Population.java
+++ src/Population.java
@@ -25,10 +25,67 @@ public class Population implements EcoSysteme, Iterable<Animal> {
 
     // TO BE COMPLETED //
 
+
     public Population( Herbe herbe, ArrayList<Animal> proies, ArrayList<Animal> predateurs ) {
-	
+
+
 	// TO BE COMPLETED //
 	
+    }
+
+    @Override
+    public int getNombreProies() {
+        return 0;
+    }
+
+    @Override
+    public int getNombrePredateurs() {
+        return 0;
+    }
+
+    @Override
+    public int getNombreProiesMatures() {
+        return 0;
+    }
+
+    @Override
+    public int getNombrePredateursMatures() {
+        return 0;
+    }
+
+    @Override
+    public int getNombreProiesChassables() {
+        return 0;
+    }
+
+    @Override
+    public double masseProies() {
+        return 0;
+    }
+
+    @Override
+    public double massePredateurs() {
+        return 0;
+    }
+
+    @Override
+    public void vieillir() {
+
+    }
+
+    @Override
+    public void chasser() {
+
+    }
+
+    @Override
+    public void reproduire() {
+
+    }
+
+    @Override
+    public void melanger() {
+
     }
 
     // TO BE COMPLETED //
