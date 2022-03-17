892068a0c42c7708b35857b6b0a7772c3b31cb56
JasLivie <101596915+JasLivie@users.noreply.github.com>
2022-03-17 11:19:24 -0400

    .

--- src/Animal.java
+++ src/Animal.java
@@ -17,89 +17,100 @@
     // TO BE COMPLETED //
 
 abstract class Animal implements ProiePredateur {
-    protected int age;
-    protected int ageMature;
-    protected boolean proie;
-    protected double facteurCroissance;
-    protected double masse;
-    protected int maxAge;
+        protected int age = 0;
+        protected int ageMature;
+        protected boolean proie;
+        protected double facteurCroissance;
+        protected double masse = 10;
+        protected int AGEMAX;
+        protected double masseAManger;
 
-    @Override
-    public void naitre() {
 
-    }
-
-    @Override
-    public void vieillir() {
-
-    }
-
-    @Override
-    public void manger() {
-
-    }
-
-    @Override
-    public Animal accoucher() {
-        return null;
-    }
-
-    @Override
-    public void mourir() {
-        if (age > ageMax){
+        @Override
+        public void naitre () {// est ce que ou on dit age de depart et masse de depart???, parce que bebe = 0 ans
+            public void naitre ( int age, double masse){
+                // ajouter l<element
+            }
         }
-        if
 
-    }
+        @Override
+        public void vieillir ( double masse, int age){
+            this.age = age + 1;
+            this.masse = masse * facteurCroissance;
 
-    @Override
-    public boolean estVivant() {
-        return false;
-    }
+        }
 
-    @Override
-    public boolean estMature() {
-        if ( age <= ageMature){
+        @Override
+        public void manger ( double masse){
+            masseAManger = masse * 2; //PAS CERTAINE; preciser si antilope ou bien herbe que l'animal mange?
+
+
+        }
+
+        @Override
+        public Animal accoucher () {
+
+            return null;
+        }
+
+//        @Override
+//        public void mourir () { // à modifier, mourir pour toutes les conditions? trop vieux, pas assez mangé...
+//            if (age > AGEMAX) {
+//            }
+//            if
+//
+//        }
+
+        @Override
+        public boolean estVivant () {
             return false;
+        } // a modifier
+
+        @Override
+        public boolean estMature () {
+            if (age <= ageMature) {
+                return false;
+            }
         }
-    }
 
-    @Override
-    public void setProie(boolean proie) {
-        this.proie = proie;
-    }
+        @Override
+        public void setProie ( boolean proie){
+            this.proie = proie;
+        }
 
-    @Override
-    public boolean estProie() {
-        return false;
-    }
+        @Override
+        public boolean estProie () {
+            return false;
+        } // a faire, difference entre setProie et estProie, car setProie retourne un false, mais c<est faux non?
+        // PARCE QUE doit pas avoir estproie et estpredateur???
 
-    @Override
-    public double getMasse() {
-        return masse;
-    }
+        @Override
+        public double getMasse () {
+            return masse;
+        }
 
-    @Override
-    public void setMasse(double masse) {
-        this.masse = masse;
-    }
+        @Override
+        public void setMasse ( double masse){
+            this.masse = masse;
+        }
 
-    @Override
-    public void setAge(int age) {
-        this.age = age;
-    }
-    @Override
-    public int getAge() {
-        return age;
-    }
+        @Override
+        public void setAge ( int age){
+            this.age = age;
+        }
+        @Override
+        public int getAge () {
+            return age;
+        }
 
-    @Override
-    public int getAgeMax() {
-        return maxAge;
-    }
+        @Override
+        public int getAgeMax () {
+            return AGEMAX;
+        }
 
-    @Override
-    public int getAgeMature() {
-        return ageMature;
+        @Override
+        public int getAgeMature () {
+            return ageMature;
+        }
     }
 }
